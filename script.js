5f6e9d5604419c39961e033ec5610c8d49518b31
benitez881 <pashakostrov.pk@gmail.com>
2022-02-17 16:49:12 +0200

    add some style and update js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const field = document.querySelector('.field')
 const cells = document.querySelectorAll('.cell')
 const resetBtn = document.querySelector('#resetButton')
+const resultHovered = document.querySelector('.results')
+const resultText = document.querySelector('.results__text')
+
 let counter = 0
 let ticArr = []
 let tacArr = []
 let urlMove
-const ticUrl = ('./assets/svg/tic.svg')
-const tacUrl = ('./assets/svg/tac.svg')
-const players = [ticArr, tacArr]
+const ticUrl = ('./assets/svg/111.svg')
+const tacUrl = ('./assets/svg/222.svg')
+let players = [ticArr, tacArr]
 const winArrays = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
 field.addEventListener('click', move)
@@ -17,8 +20,8 @@ function move(event) {
     let target = event.target.closest('div')
     if(!target) return  
     if(target.className != 'cell') return
-    if(target.style.backgroundColor == 'white') return
-    target.style.backgroundColor = 'white'
+    if(target.style.backgroundColor == '#faebd7') return
+    target.style.backgroundColor = '#faebd7'
     target.childNodes[0].src = chooseTicOrTac()
     
     // Custom array.indexOf()
@@ -29,7 +32,7 @@ function move(event) {
     }
     
     // Check for win array
-    findWinner()
+    setTimeout(() => findWinner(), 0);
 }
 
 function chooseArray(index) {
@@ -54,17 +57,28 @@ function resetGame() {
     counter = 0
     ticArr = []
     tacArr = []
-    equal = [1,3,3]
-    equal.length = 4
-    for(let i = 0; i < cells.length; i++) {
-        cells[i].childNodes[0].src = ''
-        cells[i].style.backgroundColor = 'black'
-    }
+    cells.forEach(element => {
+        element.childNodes[0].src = "";
+        element.style.backgroundColor = "#554a26";
+    })
+    players = [ticArr, tacArr]
     field.addEventListener('click', move)
+    resultHovered.style.visibility = 'hidden'
+    resultHovered.style.opacity = '0'
+    resultText.style.visibility = 'hidden'
+    resultText.style.opacity = '0'
 }
 
 function findWinner() {
     for(let twoArrays = 0; twoArrays < players.length; twoArrays++) {
+        if(ticArr.length + tacArr.length == 9) {
+           resultText.innerHTML = 'Draw'
+            field.removeEventListener('click', move)
+            resultHovered.style.visibility = 'visible'
+            resultHovered.style.opacity = '1'
+            resultText.style.visibility = 'visible'
+            resultText.style.opacity = '1'    
+        }
         let player = players[twoArrays].slice(0)
 
         if(player.length < 2) return
@@ -81,7 +95,12 @@ function findWinner() {
 
                 }
                 if(equal.length === 0) {
-                    twoArrays == 0 ? alert('First player win') : alert('Second player win')
+                    twoArrays == 0 ? resultText.innerHTML = 'First player win' : resultText.innerHTML = 'Second player win';
+                    field.removeEventListener('click', move)
+                    resultHovered.style.visibility = 'visible'
+                    resultHovered.style.opacity = '1'
+                    resultText.style.visibility = 'visible'
+                    resultText.style.opacity = '1'                    
                 }
             }
     }
